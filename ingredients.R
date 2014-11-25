9ff5db388f7b9f8c32dfee182fe0b71449a86191
hadley <h.wickham@gmail.com>
2014-11-25 08:39:51 -0600

    Remove dplyr dependency

--- ingredients.R
+++ ingredients.R
@@ -1,25 +1,24 @@
-library("dplyr")
-
-basic <- data_frame(
+basic <- data.frame(
   quantity = c(2, 3, 0.5, 2, 2, 6, 4) / 2,
   unit = c("", "oz", "tsp", "oz", "oz", "oz", "oz"),
   ingredient = c("large eggs", "granulated sugar", "freshly-grated nutmeg",
-    "brandy", "spiced rum", "whole milk", "heavy cream")
+    "brandy", "spiced rum", "whole milk", "heavy cream"),
+  stringsAsFactors = FALSE
 )
 
-variation <- data_frame(
+variation <- data.frame(
   quantity = c(2, 3, 0.5, 2, 3, 6, 4) / 2,
   unit = c("", "oz", "tsp", "oz", "oz", "oz", "oz"),
   ingredient = c("large eggs", "granulated sugar", "freshly-grated nutmeg",
-    "anejo tequila", "sherry", "whole milk", "heavy cream")
+    "anejo tequila", "sherry", "whole milk", "heavy cream"),
+  stringsAsFactors = FALSE
 )
 
 scale <- function(ingredients, quantity, unit) {
   by <- quantity * to_oz(unit) / serving_oz
     
-  ingredients %>% mutate(
-    quantity = quantity * by
-  ) %>% as.data.frame()
+  ingredients$quantity <- ingredients$quantity * by
+  ingredients
 }
 
 serving_oz <- sum(ingredients$vol, na.rm = TRUE)
