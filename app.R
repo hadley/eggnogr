e07fe41a2b1072c2a7054389df6c16c17ecfb09b
Hadley Wickham <h.wickham@gmail.com>
2024-08-09 15:06:03 -0500

    Convert to app.R

--- app.R
+++ app.R
@@ -1,14 +1,14 @@
 library(shiny)
 source("ingredients.R")
 
-shinyUI(fluidPage(
+ui <- fluidPage(
   fluidRow(
-    column(width = 4, 
+    column(width = 4,
       h1("eggnogr"),
-      p("Egg nog recipe by ", 
-        a("Jeffrey Morgenthaler", href = "http://www.jeffreymorgenthaler.com/2009/egg-nog/"), 
+      p("Egg nog recipe by ",
+        a("Jeffrey Morgenthaler", href = "http://www.jeffreymorgenthaler.com/2009/egg-nog/"),
         ".", br(),
-        "Scaling by Hadley Wickham, R, and ", 
+        "Scaling by Hadley Wickham, R, and ",
       a("shiny", href = "http://shiny.rstudio.com"), "."),
       p("How much eggnog do you want?"),
       numericInput("quantity", "", 1, min = 1),
@@ -19,7 +19,7 @@ shinyUI(fluidPage(
     ),
     column(width = 8,
       h2("Ingredients"),
-      tableOutput("ingredients"),  
+      tableOutput("ingredients"),
       p("(all units by volume, not weight)"),
       h2("Instructions"),
       tags$ol(
@@ -33,4 +33,31 @@ shinyUI(fluidPage(
     )
   ),
   p(a("Read the source", href = "https://github.com/hadley/eggnogr"))
-))
+)
+
+
+server <- function(input, output) {
+  ingredients <- reactive(
+    if (input$variation) variation else basic
+  )
+
+  scaled <- reactive({
+    df <- scale(
+      ingredients(),
+      input$quantity,
+      input$units,
+      nice = input$nice,
+      metric = input$metric
+    )
+    df$quantity <- format(df$quantity, drop0trailing = TRUE)
+    df
+  })
+
+  output$ingredients <- renderTable(
+    scaled(),
+    include.rownames = FALSE,
+    include.colnames = FALSE
+  )
+}
+
+shinyApp(ui, server)
